3cdf89264539c3c77cced4665b23b023a1fe871a
keoken <keokenppb@gmail.com>
2013-06-04 10:53:18 +0900

    8.2 done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,22 +9,25 @@ describe User do
   
   subject { @user}
   
-  it { should respond_to(:name)}
-  it { should respond_to(:email)}
-  it { should respond_to(:password_digest)}
-  it { should respond_to(:password)}
-  it { should respond_to(:password_confirmation)}
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
-  it { should be_valid}
+  it { should be_valid }
 
   describe "when name is not present" do
-    before { @user.name = ""}
-    it { should_not be_valid}
+    before { @user.name = "" }
+    it { should_not be_valid }
   end
   
   describe "when name is too long" do
-    before { @user.name = "a"*51}
-    it { should_not be_valid}
+    before { @user.name = "a"*51 }
+    it { should_not be_valid }
   end
   
   describe "when email format is invalid" do
@@ -98,6 +101,11 @@ describe User do
       it { should_not eq user_for_invalid_password}
       specify { expect(user_for_invalid_password).to be_false}
     end
+  end
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
 
