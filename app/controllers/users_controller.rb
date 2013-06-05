f64a585204ed6251ce8214d11b611747c2db17b9
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-06-05 10:32:23 +0900

    just finished to Listing 9.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
@@ -19,8 +19,13 @@ class UsersController < ApplicationController
     end
   end
 
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private 
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
