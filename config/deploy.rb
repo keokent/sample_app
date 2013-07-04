c55471ce6d7441e5fd69d24055089f69ba6bfae6
keoken <keokenppb@gmail.com>
2013-07-04 12:30:46 +0900

    デプロイ時にbundle installとUnicornの再起動をするようにした。

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,25 +11,13 @@ set :shared_children, %w(log pids system run)
 role :web, "app002.keoken.pb"         # Your HTTP server, Apache/etc
 role :app, "app002.keoken.pb"         # This may be the same as your `Web` server
 role :db,  "app002.keoken.pb", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
 
 
-
-task :clone_app do
-  run "git clone https://github.com/keokent/sample_app.git /tmp/sample_app"
+namespace :deploy do
+  desc "restart unicorn"
+  task :restart_unicorn do
+    run "/etc/init.d/unicorn_sample_app restart"
+  end
 end
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+after :deploy, "deploy:restart_unicorn"
